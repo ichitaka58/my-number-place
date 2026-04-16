aabc525b8676db228a2f37f14aa1c2e949c3f046
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-04-16 14:19:10 +0900

    fix: fetchResult戻り値の方修正、feat: Result.tsxの実装、mockResults.jsonの追加

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,32 +1,52 @@
+import { useEffect, useState } from "react";
 import { BottomNavigation } from "../components/BottomNavigation";
+import { fetchResults } from "../lib/api";
+import type { Record } from "../types";
+import { generateDisplayTimer } from "../utils/time";
+import mockData from "../mock/mockRecords.json";
 
 const Result = () => {
+  const [records, setRecords] = useState<Record[]>([]);
+  const [mockRecords] = useState(mockData);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const data = await fetchResults();
+      if (data) setRecords(data);
+    };
+    loadData();
+  }, []);
+
   return (
-      <div className="min-h-screen bg-slate-950 font-['Inter',sans-serif] text-slate-100 flex flex-col items-center justify-center pt-8 pb-24 px-4 overflow-x-hidden">
-        <div className="text-center p-10 bg-slate-900 border-2 border-cyan-400 rounded-xl shadow-[0_0_20px_rgba(34,211,238,0.5)]">
-          <h1 className="text-4xl sm:text-5xl font-extrabold mb-8 text-transparent bg-clip-text bg-linear-to-r from-cyan-400 via-blue-500 to-purple-500 tracking-wider drop-shadow-lg">
-            Result
-          </h1>
-          <div className="mb-8">
-            {/* <input
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              placeholder="Enter your name..."
-              className="w-64 p-3 text-lg bg-slate-950 text-cyan-300 border-2 border-slate-600 rounded-lg placeholder:text-slate-500 focus:outline-none focus:border-cyan-400 focus:ring-1 focus:ring-cyan-400 transition-colors"
-            /> */}
-          </div>
-          <div>
-            {/* <button
-              onClick={handleStart}
-              className="px-8 py-3 text-xl font-bold bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition-all duration-300 border border-blue-400 shadow-[0_0_10px_rgba(59,130,246,0.5)] hover:shadow-[0_0_15px_rgba(34,211,238,0.6)]"
-            >
-              Start Game
-            </button> */}
-          </div>
+    <div className="min-h-screen bg-slate-950 font-['Inter',sans-serif] text-slate-100 flex flex-col items-center justify-center pt-8 pb-24 px-4 overflow-x-hidden">
+      <div className="text-center p-10 bg-slate-900 border-2 border-cyan-400 rounded-xl shadow-[0_0_20px_rgba(34,211,238,0.5)]">
+        <h1 className="text-4xl sm:text-5xl font-extrabold mb-8 text-transparent bg-clip-text bg-linear-to-r from-cyan-400 via-blue-500 to-purple-500 tracking-wider drop-shadow-lg">
+          Result
+        </h1>
+        <div className="flex justify-center gap-4 mb-4">
+          <button>Easy</button>
+          <button>Medium</button>
+          <button>Hard</button>
         </div>
-        <BottomNavigation />
+        <ul className="space-y-2">
+          {mockRecords.map((r) => (
+            <li
+              key={r.id}
+              className="flex justify-around items-center border-b-2 border-blue-400 pb-1"
+            >
+              <span className="w-30 text-left text-sm truncate">
+                {r.userName}
+              </span>
+              <span className="w-12 text-right text-cyan-400 tabular-nums [text-shadow:0_0_10px_rgba(34,211,238,0.8)]">
+                {generateDisplayTimer(r.time)}
+              </span>
+              <span className="w-20 text-center text-sm">{r.level}</span>
+            </li>
+          ))}
+        </ul>
       </div>
+      <BottomNavigation />
+    </div>
   );
 };
 
